fd025a758383f400302f7ce1f895bf46ea212b50
Cao Van Cuong <vancuong.itf@gmail.com>
2026-05-14 10:42:23 +0700

    update pending post struct

--- src/main/kotlin/posts/PendingPost.kt
+++ src/main/kotlin/posts/PendingPost.kt
@@ -7,4 +7,11 @@ data class PendingPost(
     val id: String,
     val imagePath: String,
     val imageExt: String,
+    val pendingBlocks: List<PendingBlock>? = null,
+)
+
+@Serializable
+data class PendingBlock(
+    val id: String,
+    val imagePath: String,
 )
